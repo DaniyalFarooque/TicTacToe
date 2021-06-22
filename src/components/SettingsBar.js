81e9c9153484804a5089d74a40367b5b0541f35a
Daniyal Farooque <danf7861@gmail.com>
2021-06-22 18:52:27 +0530

    Update SettingsBar.js

--- src/components/SettingsBar.js
+++ src/components/SettingsBar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Card, Button} from '@material-ui/core';
+import {Card, Button, Grid} from '@material-ui/core';
 import {ToggleButton, ToggleButtonGroup} from '@material-ui/lab';
 import ComputerIcon from '@material-ui/icons/Computer';
 import PersonIcon from '@material-ui/icons/Person';
@@ -8,11 +8,10 @@ const SettingsBar = ({opponent,startPlayer,winner,xO,onChange,status,onClick}) =
     const style = {
         display: 'block',
         height: '450px',
-        backgroundColor: 'grey',
-        boxShadow: 'none',
-        width: '200px'
+        backgroundColor: 'transparent',
+        width: '150px'
+        // boxShadow: 'none'
     };
-    
     let message;
     if(winner===null){
         message = `Next Player:  ${xO}`;
@@ -24,9 +23,9 @@ const SettingsBar = ({opponent,startPlayer,winner,xO,onChange,status,onClick}) =
 
     return (
         <>
-            <div className="setbar">
-                <Card style = {style}>
-                    <h3>Opponent</h3>
+            <Card style = {style}>
+                <h3>Opponent</h3>
+                <Grid container justify="center" alignItems="center" item>
                     <ToggleButtonGroup value={opponent} exclusive onChange={onChange.handleOpponent}>
                         <ToggleButton value="human" aria-label="human" color="primary">
                             <PersonIcon />
@@ -35,24 +34,24 @@ const SettingsBar = ({opponent,startPlayer,winner,xO,onChange,status,onClick}) =
                             <ComputerIcon />
                         </ToggleButton>
                     </ToggleButtonGroup>
+                <h3>Starting Player</h3>
+                <ToggleButtonGroup value={startPlayer} exclusive  onChange={onChange.handleStartPlayer}>
+                    <ToggleButton value="human" disabled = {status} aria-label="human" color="primary">
+                        <PersonIcon />
+                    </ToggleButton>
+                    <ToggleButton value="computer" disabled = {status} aria-label="computer" color="primary">
+                        <ComputerIcon />
+                    </ToggleButton>
+                </ToggleButtonGroup>
+            
+                <h3>{message}</h3> 
 
-                    <h3>Starting Player</h3>
-                    <ToggleButtonGroup value={startPlayer} exclusive  onChange={onChange.handleStartPlayer}>
-                        <ToggleButton value="human" disabled = {status} aria-label="human" color="primary">
-                            <PersonIcon />
-                        </ToggleButton>
-                        <ToggleButton value="computer" disabled = {status} aria-label="computer" color="primary">
-                            <ComputerIcon />
-                        </ToggleButton>
-                    </ToggleButtonGroup>
-                
-                    <h3>{message}</h3> 
+                <Button style = {{ background: 'linear-gradient(45deg, #ef476f 30%, #ffafcc 90%)'}} variant="contained" onClick = {onClick} >
+                    New Game
+                </Button>
+                </Grid>
 
-                    <Button variant="contained" onClick = {onClick} color="primary">
-                        New Game
-                    </Button>
-                </Card>
-            </div>
+            </Card>
         </>
     );
 }
