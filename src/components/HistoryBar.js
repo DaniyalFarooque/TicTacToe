f0e40d854a34d8330218158bc98f669b35925eec
Daniyal Farooque <danf7861@gmail.com>
2021-06-22 16:48:05 +0530

    Create HistoryBar

---
+++ src/components/HistoryBar.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {Card} from '@material-ui/core';
+import Move from './Move';
+
+const HistoryBar = ({history,onClick})=>{
+    const style = {
+        backgroundColor: 'grey',
+        boxShadow: 'none',
+    };
+    
+    return (
+        <>
+            <Card style = {style}>
+                <h3>History</h3>
+                <div className="info-wrapper">
+                    {history.map((_step, move) => {
+                        const destination = move ? `Go to move #${move}` : "Go to Start";
+                        return (
+                        <Move move={move} destination={destination} onClick={onClick}/>
+                        );
+                    })}
+                </div>
+            </Card>
+        </>
+    )
+}
+
+export default HistoryBar;
